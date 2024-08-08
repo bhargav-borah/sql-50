64aacda604392df3966087d8ec3c0e0625ea5eac
bhargav_borah <100614703+bhargav-borah@users.noreply.github.com>
2024-08-09 00:12:10 +0530

    Update 9_197_rising_temperature.sql

--- 9_197_rising_temperature.sql
+++ 9_197_rising_temperature.sql
@@ -4,3 +4,10 @@ FROM Weather t1
 LEFT JOIN Weather t2
 ON t1.recordDate = DATE_ADD(t2.recordDate, INTERVAL 1 DAY)
 WHERE t2.id IS NOT NULL AND t1.temperature > t2.temperature;
+
+-- SELECT
+--     t1.id
+-- FROM Weather t1
+-- LEFT JOIN Weather t2
+-- ON t1.recordDate = DATE_ADD(t2.recordDate, INTERVAL 1 DAY)
+-- WHERE t1.temperature > t2.temperature;
