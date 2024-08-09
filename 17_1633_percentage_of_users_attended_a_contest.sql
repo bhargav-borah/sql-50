97dcd1000407d4d6ef83d049179230814b5767cd
bhargav_borah <100614703+bhargav-borah@users.noreply.github.com>
2024-08-09 11:23:52 +0530

    Create 17_1633_percentage_of_users_attended_a_contest.sql

---
+++ 17_1633_percentage_of_users_attended_a_contest.sql
@@ -0,0 +1,8 @@
+SELECT
+    r.contest_id,
+    ROUND((COUNT(r.contest_id) / (SELECT COUNT(*) FROM Users)) * 100, 2) AS percentage
+FROM Register r
+LEFT JOIN Users u
+ON r.user_id = u.user_id
+GROUP BY r.contest_id
+ORDER BY percentage DESC, r.contest_id ASC;
