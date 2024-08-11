fd691ab500591d3d74cef62ad63622375e26a038
bhargav_borah <100614703+bhargav-borah@users.noreply.github.com>
2024-08-11 11:12:12 +0530

    Update 21_550_game_play_analysis_IV.sql

--- 21_550_game_play_analysis_IV.sql
+++ 21_550_game_play_analysis_IV.sql
@@ -12,3 +12,15 @@ FROM Activity a
 JOIN cte c
 ON a.player_id = c.player_id
 WHERE DATE_ADD(c.first_login_date, INTERVAL 1 DAY) = a.event_date;
+
+-- SELECT 
+--     ROUND(COUNT(*) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
+-- FROM Activity
+-- WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY))
+--     IN (
+--         SELECT
+--             player_id,
+--             MIN(event_date) AS first_login_date
+--         FROM Activity
+--         GROUP BY player_id
+--     )
